eeb28261e10ef23a3209b63dce0f8eba35166862
UmamaMasroor <umamafatima319@gmail.com>
2024-10-02 22:36:47 +0500

    assignment 3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Header from './components/header/page'
-
+import Footer from './components/footer/page'
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -32,6 +32,7 @@ export default function RootLayout({
       >
         <Header/>
         {children}
+    <Footer/>
       </body>
     </html>
   );
